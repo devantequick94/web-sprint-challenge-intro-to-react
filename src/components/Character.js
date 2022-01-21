0bb7b29f72991ed88df6793e512d78e28786b19b
devantequick <devantequick94@gmail.com>
2022-01-21 05:44:27 -0500

    adjusting code for codegrade

--- src/components/Character.js
+++ src/components/Character.js
@@ -11,8 +11,6 @@ const DisplayCharacterCard = (props) => {
         
           (
             <article>
-              <img src={person.img} alt={person.name}></img>
-              <h2>{person.name}</h2>
               <p>{`${person.name} is a ${person.gender} ${person.species} who is currently ${person.status}`}</p>
             </article> 
           ) 

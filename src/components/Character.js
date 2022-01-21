d377dac5c6f8ee4a88feae1c59024addb53b8225
devantequick <devantequick94@gmail.com>
2022-01-21 04:45:31 -0500

    added props

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,15 @@
-// Write your Character component here
+import React from "react";
+
+
+const DisplayCharacterCard = (props) => {
+
+    console.log(props
+        )
+    return (
+        <div>
+      testing 123
+        </div>
+    )
+}
+
+export default DisplayCharacterCard;
